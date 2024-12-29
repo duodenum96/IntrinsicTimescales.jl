91f5547149d9703c30504b710aa69f331564eddb
duodenum96 <catalyasir@gmail.com>
2024-12-29 18:45:24 +0300

    implement DifferentialEquations.jl backend for OU

--- src/models/ou_process.jl
+++ src/models/ou_process.jl
@@ -7,11 +7,11 @@ using Distributions
 using BayesianINT
 using ..Models
 using NonlinearSolve
+import DifferentialEquations as deq
 
 export OneTimescaleModel, generate_ou_process, informed_prior_one_timescale
-
 """
-Generate an Ornstein-Uhlenbeck process with a single timescale.
+Generate an Ornstein-Uhlenbeck process with a single timescale with vanilla Julia code.
 
 # Arguments
 - `tau::Float64`: Timescale
@@ -20,20 +20,63 @@ Bayesian inference doesn't have to deal with it.
 - `deltaT::Float64`: Time step size
 - `T::Float64`: Total time length
 - `num_trials::Int64`: Number of trials/trajectories to generate
+- `backend::String`: Backend to use. Must be 'vanilla' or 'sciml'.
+- `parallel::Bool`: Whether to use parallelization. Only works with 'sciml' backend.
 
 # Returns
 - Matrix{Float64}: Generated OU process data with dimensions (num_trials, num_timesteps)
 
 The process is generated using the Euler-Maruyama method with the specified time step deltaT.
 """
-function generate_ou_process(
-    tau::Float64,
+function generate_ou_process(tau::Union{Float64, Vector{Float64}},
+                            true_D::Float64,
+                            deltaT::Float64,
+                            T::Float64,
+                            num_trials::Int64;
+                            backend::String="sciml",
+                            parallel::Bool=false)
+    if backend == "vanilla"
+        return generate_ou_process_vanilla(tau, true_D, deltaT, T, num_trials)
+    elseif backend == "sciml"
+        return generate_ou_process_sciml(tau, true_D, deltaT, T, num_trials, parallel)
+    else
+        error("Invalid backend: $backend. Must be 'vanilla' or 'sciml'.")
+    end
+end
+
+"""
+Generate an Ornstein-Uhlenbeck process with a single timescale using DifferentialEquations.jl.
+"""
+function generate_ou_process_sciml(
+    tau::Union{Float64, Vector{Float64}},
+    true_D::Float64,
+    deltaT::Float64,
+    T::Float64,
+    num_trials::Int64,
+    parallel::Bool=false
+)
+    f = (du, u, p, t) -> du .= -u ./ p[1]
+    g = (du, u, p, t) -> du .= 1.0 # Handle the variance below
+    p = (tau, true_D)
+    u0 = randn(num_trials) # Quick hack instead of ensemble problem
+    prob = deq.SDEProblem(f, g, u0, (0.0, T), p)
+    times = deltaT:deltaT:T
+    sol = deq.solve(prob; saveat=times)
+    sol_matrix = reduce(hcat, sol.u)
+    ou_scaled = ((sol_matrix .- mean(sol_matrix, dims=2)) ./ std(sol_matrix, dims=2)) * true_D
+    return ou_scaled
+end
+
+"""
+Generate an Ornstein-Uhlenbeck process with a single timescale using vanilla Julia code.
+"""
+function generate_ou_process_vanilla(
+    tau::Union{Float64, Vector{Float64}},
     true_D::Float64,
     deltaT::Float64,
     T::Float64,
     num_trials::Int64
 )
-    D_normalized = 1 / tau
     num_bin = Int(T / deltaT)
     noise = randn(num_trials, num_bin)
     ou = zeros(num_trials, num_bin)
@@ -41,11 +84,11 @@ function generate_ou_process(
 
     for i in 2:num_bin
         ou[:, i] = @views ou[:, i-1] .- (ou[:, i-1] / tau) * deltaT .+
-                           sqrt(2*D_normalized*deltaT) * noise[:, i-1]
+            sqrt(deltaT) * noise[:, i-1]
     end
-    # Scale the data by the true variance
-    ou = ou * sqrt(true_D)
-    return ou
+    ou_scaled = ((ou .- mean(ou, dims=2)) ./ std(ou, dims=2)) * true_D
+
+    return ou_scaled
 end
 
 function informed_prior_one_timescale(data::AbstractMatrix)
@@ -74,15 +117,16 @@ end
 
 function Models.generate_data(model::OneTimescaleModel, theta)
     tau = theta
-    return generate_ou_process(tau, model.data_var, model.deltaT, model.T, model.numTrials)
+    return generate_ou_process(tau, model.data_var, model.deltaT, model.T, model.numTrials; backend="sciml", parallel=false)
 end
 
-function Models.summary_stats(model::OneTimescaleModel, data)
-    return comp_ac_fft(data)
+function Models.summary_stats(model::OneTimescaleModel, data; n_lags=3000)
+    return comp_ac_fft(data; n_lags=n_lags)
 end
 
-function Models.distance_function(model::OneTimescaleModel, sum_stats, data_sum_stats)
-    return linear_distance(sum_stats, data_sum_stats)
+function Models.distance_function(model::OneTimescaleModel, sum_stats, data_sum_stats; n_lags=3000)
+    # TODO: Implement n_lags throughout the codebase
+    return linear_distance(sum_stats[1:n_lags], data_sum_stats[1:n_lags])
 end
 
 end # module OrnsteinUhlenbeck
