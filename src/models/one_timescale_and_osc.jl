3413d8e8ba5008651a47362ac235290f52cc95dd
duodenum96 <catalyasir@gmail.com>
2024-12-30 20:52:13 -0500

    switch to PSD and logarithmci distance, do rescaling of priors to not go insane

--- src/models/one_timescale_and_osc.jl
+++ src/models/one_timescale_and_osc.jl
@@ -6,6 +6,7 @@
 module OneTimescaleAndOsc
 
 using Distributions
+using Statistics
 using ..Models
 using ..OrnsteinUhlenbeck
 using BayesianINT
@@ -13,7 +14,8 @@ using BayesianINT
 export OneTimescaleAndOscModel
 
 """
-One-timescale OU process model
+One-timescale OU process model + Additive oscillation
+Theta: [tau, freq, coeff]
 """
 struct OneTimescaleAndOscModel <: AbstractTimescaleModel
     data::Matrix{Float64}
@@ -23,22 +25,76 @@ struct OneTimescaleAndOscModel <: AbstractTimescaleModel
     dt::Float64
     T::Float64
     numTrials::Int
+    data_mean::Float64
     data_var::Float64
-    n_lags::Int
+    prior_scales::Vector{Tuple{Float64, Float64}} # a, b - a (to unit scale: y= (x-a)/(b-a); to original scale: x = y*(b-a) + a)
+end
+
+"""
+Handle the scaling
+"""
+function OneTimescaleAndOscModel(data::Matrix{Float64}, prior::Vector{Distribution},
+                                 data_sum_stats::Vector{Float64}, epsilon::Float64,
+                                 dt::Float64, T::Float64, numTrials::Int,
+                                 data_mean::Float64, data_var::Float64)
+    # Internally rescale the priors to a unit scale
+    prior_unit = [Uniform(0.0, 1.0) for _ in prior]
+    # Get scales of original priors
+    prior_scales = [(prior[i].a, prior[i].b - prior[i].a) for i in 1:length(prior)]
+    return OneTimescaleAndOscModel(data, prior_unit, data_sum_stats, epsilon, dt, T,
+                                   numTrials,
+                                   data_mean, data_var, prior_scales)
 end
 
 # Implementation of required methods
-function Models.generate_data(model::OneTimescaleModel, theta)
-    tau,  = theta
-    return generate_ou_process(tau, model.data_var, model.dt, model.T, model.numTrials; backend="sciml")
+function Models.generate_data(model::OneTimescaleAndOscModel, theta)
+    return generate_ou_with_oscillation(theta, model.dt, model.T, model.numTrials,
+                                        model.data_mean, model.data_var)
 end
 
-function Models.summary_stats(model::OneTimescaleModel, data)
-    return comp_ac_fft(data; n_lags=model.n_lags)
+function Models.summary_stats(model::OneTimescaleAndOscModel, data)
+    return comp_psd(data, 1 / model.dt)[1] # Remove DC component
 end
 
-function Models.distance_function(model::OneTimescaleModel, sum_stats, data_sum_stats)
-    return linear_distance(sum_stats, data_sum_stats)
+function Models.distance_function(model::OneTimescaleAndOscModel, sum_stats, data_sum_stats)
+    if any(isnan.(sum_stats)) || any(isnan.(data_sum_stats))
+        return 1e5
+    else
+        return logarithmic_distance(sum_stats, data_sum_stats)
+    end
 end
 
-end # module OneTimescale 
+function Models.generate_data_and_reduce(model::OneTimescaleAndOscModel, theta)
+    # The scales of the parameters (tau and freq) are very different. This might cause numerical
+    # problems in the ABC algorithm. To avoid this, we rescale the parameters to be on the same scale.
+    # We scale the priors to a unit scale and then rescale the parameters to the original scale.
+    # We also rescale the data to a unit scale.
+    theta_rescaled = Models.rescale_theta(model, theta)
+
+    synth = generate_data(model, theta_rescaled)
+    sum_stats = summary_stats(model, synth)
+    d = distance_function(model, sum_stats, model.data_sum_stats)
+    return d
+end
+
+function Models.rescale_theta(model::OneTimescaleAndOscModel, theta)
+    return [theta[i] * model.prior_scales[i][2] + model.prior_scales[i][1]
+            for i in eachindex(theta)]
+end
+
+function Models.bayesian_inference(model::BaseModel, epsilon_0=0.5,
+                                   min_samples=100,
+                                   steps=60,
+                                   minAccRate=0.001,
+                                   max_iter=500)
+    results = pmc_abc(model;
+                      epsilon_0=epsilon_0,
+                      min_samples=min_samples,
+                      steps=steps,
+                      minAccRate=minAccRate,
+                      max_iter=max_iter)
+    results.theta_accepted = rescale_theta(model, results.theta_accepted)
+    return results
+end
+
+end # module OneTimescaleAndOsc
