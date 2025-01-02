694cff17495fa2adfac5ceb33d00508d7edc0358
duodenum96 <catalyasir@gmail.com>
2025-01-02 15:17:29 -0500

    small bug fixes in example

--- examples/one_timescale_and_osc_vi.jl
+++ examples/one_timescale_and_osc_vi.jl
@@ -12,9 +12,9 @@ import DifferentialEquations as deq
 true_tau = 300.0 / 1000.0  # timescale in seconds
 true_freq = 10.0  # Hz
 true_coeff = 0.95  # mixing coefficient
-num_trials = 30
+num_trials = 20
 T = 10.0
-dt = 1 / 500
+dt = 1 / 125
 times = dt:dt:T
 nlags = 100
 frange = [0, 50]
@@ -31,6 +31,11 @@ freq_idx = findall(freq .>= frange[1] .&& freq .<= frange[2])
 data_psd = data_psd[freq_idx]
 freq = freq[freq_idx]
 
+log_psd = log10.(data_psd)
+mean_psd = mean(log_psd)
+sd_psd = std(log_psd)
+log_psd_scaled = (log_psd .- mean_psd) * sd_psd
+
 """
 Model for fitting PSD of OU process with oscillation
 data: PSD values
@@ -39,43 +44,52 @@ fs: sampling frequency
 """
 tr.@model function fit_psd_with_osc(data, freq, fs)
     # Priors
-    tau ~ tr.truncated(tr.Normal(0.5, 0.5), 0.0, Inf)
-    freq_osc ~ tr.truncated(tr.Normal(20.0, 5.0), 0.0, Inf)
-    coeff ~ tr.truncated(tr.Normal(0.5, 0.3), 0.0, 1.0)
+    tau ~ tr.truncated(tr.Normal(0.5, 0.1), 0.0, Inf)
+    # freq_osc ~ tr.truncated(tr.Normal(10.0, 5.0), 0.0, Inf)
+    # Frequency is on a different scale. We need to transform it.
+    freq_osc ~ tr.truncated(tr.Normal(0.5, 0.05), 0.0, Inf)
+    coeff ~ tr.truncated(tr.Normal(0.5, 0.1), 0.0, 1.0)
     σ ~ tr.Exponential(1)
+
+    transformed_freq = freq_osc * 20
     
     # Generate predicted data
-    theta = [tau, freq_osc, coeff]
+    theta = [tau, transformed_freq, coeff]
+    println("theta: $theta")
     predicted_ts = generate_ou_with_oscillation(theta, dt, T, num_trials, data_mean, data_var)
     predicted_psd, _ = comp_psd_adfriendly(predicted_ts, fs)
-    predicted_psd_sel = predicted_psd[freq_idx]
+    predicted_psd_sel = log10.(predicted_psd[freq_idx])
+    # predicted_psd_scaled = (predicted_psd_sel .- mean_psd) * sd_psd
 
     # Likelihood
+    # data ~ tr.MvNormal(predicted_psd_scaled, σ^2)
     data ~ tr.MvNormal(predicted_psd_sel, σ^2)
 
     return nothing
 end
 
 # Fit model using ADVI
-model = fit_psd_with_osc(data_psd, freq, fs)
-advi = tr.ADVI(10, 100, AutoZygote())
+model = fit_psd_with_osc(log_psd, freq, fs)
+advi = tr.ADVI(10, 300, AutoZygote())
 chain = tr.vi(model, advi)
 
 # Get inferred parameters
-inferred_params = mean(rand(chain, 4000); dims=2)
+inferred_params = median(rand(chain, 4000); dims=2)
+inferred_params[2] = inferred_params[2] * 20
 inferred_params_var = var(rand(chain, 4000); dims=2)
 
 # Generate predictions with inferred parameters
 theta_pred = [inferred_params[1], inferred_params[2], inferred_params[3]]
 predicted_ts = generate_ou_with_oscillation(theta_pred, dt, T, num_trials, data_mean, data_var)
-predicted_psd, _ = comp_psd(predicted_ts, fs)
+predicted_psd, _ = comp_psd_adfriendly(predicted_ts, fs)
 predicted_psd_sel = predicted_psd[freq_idx]
 
 # Get parameter draws from chain
 chain_draws = rand(chain, 4000)
 tau_draws = chain_draws[1,:][:]
-freq_draws = chain_draws[2,:][:]
+freq_draws = chain_draws[2,:][:] * 20
 coeff_draws = chain_draws[3,:][:]
+
 
 # Randomly select some parameter combinations for uncertainty visualization
 n_samples = 20
