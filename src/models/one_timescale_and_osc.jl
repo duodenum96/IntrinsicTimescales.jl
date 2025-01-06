48755647feeb315d76ceb60ffc6cd52879641b56
duodenum96 <catalyasir@gmail.com>
2025-01-06 05:07:38 +0300

    get rid of prior scaling

--- src/models/one_timescale_and_osc.jl
+++ src/models/one_timescale_and_osc.jl
@@ -29,26 +29,6 @@ struct OneTimescaleAndOscModel <: AbstractTimescaleModel
     numTrials::Int
     data_mean::Float64
     data_var::Float64
-    prior_scales::Vector{Tuple{Float64, Float64}} # a, b - a (to unit scale: y= (x-a)/(b-a); to original scale: x = y*(b-a) + a)
-end
-
-"""
-Handle the scaling using outer constructor
-"""
-function OneTimescaleAndOscModel(data::Matrix{Float64},
-                                 prior::Union{Vector{Uniform{Float64}},
-                                              Vector{Distribution}},
-                                 data_sum_stats::Vector{Float64}, epsilon::Float64,
-                                 dt::Float64, T::Float64, numTrials::Int,
-                                 data_mean::Float64, data_var::Float64)
-    # Internally rescale the priors to a unit scale
-    prior = convert(Vector{Distribution}, prior)
-    prior_unit = [Uniform(0.0, 1.0) for _ in prior]
-    # Get scales of original priors
-    prior_scales = [(prior[i].a, prior[i].b - prior[i].a) for i in 1:length(prior)]
-    return OneTimescaleAndOscModel(data, prior_unit, data_sum_stats, epsilon, dt, T,
-                                   numTrials,
-                                   data_mean, data_var, prior_scales)
 end
 
 # Implementation of required methods
@@ -60,35 +40,40 @@ end
 """
 Compute combined distance using PSD shape and peak location
 """
-function combined_distance(model_psd::Vector{Float64}, 
-                         data_psd::Vector{Float64}, 
-                         freqs::Vector{Float64};
-                         peak_weight::Float64=0.3,
-                         min_freq::Float64=5.0 / 1000.0,
-                         max_freq::Float64=50.0 / 1000.0)
+function combined_distance(model_psd::Vector{Float64},
+                           data_psd::Vector{Float64},
+                           freqs::Vector{Float64};
+                           peak_weight::Float64=0.4,
+                           knee_weight::Float64=0.4,
+                           psd_weight::Float64=0.2,
+                           min_freq::Float64=2.0 / 1000.0,
+                           max_freq::Float64=50.0 / 1000.0)
     # 1. Regular PSD distance
     psd_dist = logarithmic_distance(model_psd, data_psd)
-    
+
     # 2. Peak frequency distance
-    model_peak = Utils.find_oscillation_peak(model_psd, freqs, 
-                                     min_freq=min_freq, 
-                                     max_freq=max_freq)
-    data_peak = Utils.find_oscillation_peak(data_psd, freqs, 
-                                    min_freq=min_freq, 
-                                    max_freq=max_freq)
-    
-    # Handle case where no peak is found
-    if isnan(model_peak) || isnan(data_peak)
+    model_peak = Utils.find_oscillation_peak(model_psd, freqs,
+                                             min_freq=min_freq,
+                                             max_freq=max_freq)
+    data_peak = Utils.find_oscillation_peak(data_psd, freqs,
+                                            min_freq=min_freq,
+                                            max_freq=max_freq)
+
+    model_knee = Utils.find_knee_frequency(model_psd, freqs)
+    data_knee = Utils.find_knee_frequency(data_psd, freqs)
+
+    # Handle case where no peak or knee is found
+    if isnan(model_peak) || isnan(data_peak) || isnan(model_knee) || isnan(data_knee)
         return 1e5
     end
-    
+
     # Normalize peak distance relative to frequency range
     freq_range = max_freq - min_freq
     peak_dist = abs(model_peak - data_peak) / freq_range
-    @infiltrate
+    knee_dist = abs(model_knee - data_knee) / freq_range
     # Combine distances with weighting
-    total_dist = (1.0 - peak_weight) * psd_dist + peak_weight * peak_dist
-    
+    total_dist = psd_weight * psd_dist + peak_weight * peak_dist + knee_weight * knee_dist
+
     return total_dist
 end
 
@@ -96,7 +81,7 @@ end
 Modified summary_stats to return both PSD and frequencies
 """
 function Models.summary_stats(model::OneTimescaleAndOscModel, data)
-    return comp_psd(data, 1 / model.dt) # Remove DC component
+    return comp_psd(data, 1 / model.dt)
 end
 
 """
@@ -106,24 +91,11 @@ function Models.distance_function(model::OneTimescaleAndOscModel, sum_stats, dat
     if any(isnan.(sum_stats[1])) || any(isnan.(data_sum_stats))
         return 1e5
     else
-        return combined_distance(sum_stats[1], 
-                               data_sum_stats, 
-                               sum_stats[2])
+        return combined_distance(sum_stats[1], data_sum_stats, sum_stats[2]) # sum_stats[2] is the frequencies
     end
 end
 
-function Models.rescale_theta(model::OneTimescaleAndOscModel, theta)
-    return [theta[i] * model.prior_scales[i][2] + model.prior_scales[i][1]
-            for i in eachindex(theta)]
-end
-
 function Models.generate_data_and_reduce(model::OneTimescaleAndOscModel, theta)
-    # The scales of the parameters (tau and freq) are very different. This might cause numerical
-    # problems in the ABC algorithm. To avoid this, we rescale the parameters to be on the same scale.
-    # We scale the priors to a unit scale and then rescale the parameters to the original scale.
-    # We also rescale the data to a unit scale.
-    # theta_rescaled = Models.rescale_theta(model, theta)
-
     synth = Models.generate_data(model, theta)
     sum_stats = Models.summary_stats(model, synth)
     d = Models.distance_function(model, sum_stats, model.data_sum_stats)
@@ -141,10 +113,7 @@ function Models.bayesian_inference(model::OneTimescaleAndOscModel; epsilon_0=0.5
                       steps=steps,
                       minAccRate=minAccRate,
                       max_iter=max_iter)
-    
-    for i_step in eachindex(results)
-        results[i_step].theta_accepted = Models.rescale_theta(model, results[i_step].theta_accepted)
-    end
+
     return results
 end
 
