d88efb809c1ca00d4370bcb89aa59f14a63cccef
duodenum96 <catalyasir@gmail.com>
2025-02-18 20:42:53 -0500

    update docs one final time, move bayesian tests to end

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,11 @@ using Test
     # Include all test files
     include("test_summary_stats.jl")
     include("test_distances.jl")
+    include("test_abc.jl")
+    include("test_ou_process.jl")
+    include("test_acw.jl")
     include("test_one_timescale.jl")
     include("test_one_timescale_and_osc.jl")
     include("test_one_timescale_with_missing.jl")
     include("test_one_timescale_and_osc_with_missing.jl")
-    include("test_abc.jl")
-    include("test_ou_process.jl")
-    include("test_acw.jl")
 end
